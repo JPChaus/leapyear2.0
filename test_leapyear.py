e51c1d09fa7aaae540f650d7b3b56dd6736fd0d1
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-06-01 00:38:59 -0400

    wrote fourth test, testing not divisible by 400, 100, or 4    def test_divFour(self):

--- test_leapyear.py
+++ test_leapyear.py
@@ -10,6 +10,9 @@ class TestCase(unittest.TestCase):
     
     def test_isLeapYear4(self):
         self.assertEqual(leapyear.calcLeapYear(2004), "2004 is a Leap Year.")
+    
+    def test_divFour(self):
+        self. assertEqual(leapyear.calcLeapYear(2001), "2001 is not a Leap Year.")
 
 if __name__ == '__main__':
     unittest.main()
