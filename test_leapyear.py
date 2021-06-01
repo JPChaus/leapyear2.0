4163f4fb67dee08bc069cc9aaa5af3d9a266bdc4
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-06-01 00:33:25 -0400

    wrote first test, testing divisible by 100

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,5 +5,8 @@ class TestCase(unittest.TestCase):
     def test_divFour(self):
         self. assertEqual(leapyear.calcLeapYear(2000), "2000 is a Leap Year.")
 
+    def test_divFour(self):
+        self. assertEqual(leapyear.calcLeapYear(1900), "1900 is not a Leap Year.")
+
 if __name__ == '__main__':
     unittest.main()
